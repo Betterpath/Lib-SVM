d05a3f603554c6a4edea04cd57797b89ebd0a9fc
Brandon Stiles <me@brandonstil.es>
2017-11-26 15:55:30 -0500

    devops(setup): Added setup.py file

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import subprocess
 
 from setuptools import setup
 
-class Build(distutils.cmd.Command):
+class MyBuild(distutils.cmd.Command):
     def run(self):
         protoc_command = ["make"]
         if subprocess.call(protoc_command) != 0:
             sys.exit(-1)
         distutils.command.build.run(self)
 
-setup(cmdclass={'build': Build,})
+setup(cmdclass={'build': MyBuild})
