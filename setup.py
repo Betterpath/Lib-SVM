019198654045890aa5367f6230ea94c1172e9db3
Brandon Stiles <me@brandonstil.es>
2017-11-26 15:54:19 -0500

    devops(setup): Added setup.py file

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-import sys
+import distutils.cmd
+import setuptools
+import subprocess
 
-from setuptools import setup
-import distutils.command.build
 
-class Build(distutils.command.build):
+class Build(distutils.cmd.Command):
     def run(self):
         protoc_command = ["make"]
         if subprocess.call(protoc_command) != 0:
             sys.exit(-1)
-        print "Hi"
         distutils.command.build.run(self)
 
 setup(cmdclass={'build': Build,})
-# setup(packages=['libsvm'])
