0c66226add4f3e3253f84a775da4b6848da514ea
Brandon Stiles <me@brandonstil.es>
2017-11-26 15:39:21 -0500

    devops(setup): Added setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+
+from setuptools import setup
+from setuptools.command.build import build
+
+class Build(build):
+    """Customized setuptools build command - builds protos on build."""
+    def run(self):
+        protoc_command = ["make", "python"]
+        if subprocess.call(protoc_command) != 0:
+            sys.exit(-1)
+        build.run(self)
+
+
+setup(
+    cmdclass={
+        'build': Build,
+    }
+)
